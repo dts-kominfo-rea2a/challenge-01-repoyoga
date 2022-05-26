f2eb1496dcf438648216fa087636eb3f7b99d389
Restiawan Yoga Pratama <restiawan008@gmail.com>
2022-05-26 21:05:35 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+// Monica
+const firstColor = ["Yellow", "Pink", "White", "Purple"];
+const firstFood = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"];
+const firstUser = {
+    name : "Monica",
+    gender : "Female",
+    age : 17,
+    email : "monica@dingdong.com",
+    favoriteColor : [...new Set(firstColor)],
+    isHavePet : "yes",
+    education : [
+        {name : "SD 01", city : "Jakarta", graduate : 2016},
+        {name : "SMP 02", city : "Jakarta", graduate : 2019},
+        {name : "SMA 03", city : "Jakarta"},
+    ],
+    favoriteRestaurant : [...new Set(firstFood)]
+};
+
+// Wendy
+const secColor = ["Blue", "Black", "Grey"];
+const secFood = ["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"];
+const secondUser = {
+    name : "Wendy",
+    gender : "Male",
+    age : 23,
+    email : "wendy@dingdong.com",
+    favoriteColor : [...new Set(secColor)],
+    isHavePet : "no",
+    education : [
+        {name : "SD 02", city : "Jakarta", graduate : 2010},
+        {name : "SMP 03", city : "Bogor", graduate : 2013},
+        {name : "Universitas Maju", city : "Tangerang"},
+    ],
+    favoriteRestaurant : [...new Set(secFood)]
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
